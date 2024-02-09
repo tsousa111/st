2083c7014ff2371c9c4aa85c94c207d1e9adba1e
tiago <tiagao2001@hotmail.com>
2024-02-09 21:29:29 +0000

    font size increased

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font Mono:pixelsize=14:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "FiraCode Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
