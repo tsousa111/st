7c8c5507a519864d35ef9a970480149cabfbc076
tiago <tiagao2001@hotmail.com>
2024-03-17 15:57:14 +0000

    roboto font and anysize patch

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Roboto Mono Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
